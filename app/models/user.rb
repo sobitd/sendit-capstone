0c5d4bacc8da4a2e8fbe596fc1ec6d2a2a84b2d5
sobitd <sobitdaniel@gmail.com>
2022-10-30 22:29:49 +0300

    add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :parcels
+  has_many :parcels, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name,  presence: true
   validates :email_address, presence: true, uniqueness: true

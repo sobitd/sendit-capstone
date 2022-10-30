f5ddf6b8c0800e7a3baf5e3f60469c90d91cbd58
sobitd <sobitdaniel@gmail.com>
2022-10-30 21:46:11 +0300

    create Parcel scaffold

---
+++ db/migrate/20221030184436_create_parcels.rb
@@ -0,0 +1,15 @@
+class CreateParcels < ActiveRecord::Migration[7.0]
+  def change
+    create_table :parcels do |t|
+      t.string :recipient_address
+      t.string :recipient_name
+      t.string :recipient_name
+      t.integer :weight
+      t.string :from
+      t.string :destination
+      t.integer :totalcost
+
+      t.timestamps
+    end
+  end
+end
